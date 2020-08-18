dd19f4ded1b8b5de1b2797bfb887ab281e4a8ea3
Mayur Patil <mayur.patil@kevit.io>
2020-08-18 20:17:06 +0530

    Added error screen

--- lib/routes/routes.dart
+++ lib/routes/routes.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:movie_app/error/error_screen.dart';
 
 Route<dynamic> generateRoute(RouteSettings settings) {
   switch (settings.name) {
